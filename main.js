bd1707d270a9f13fa209cf9f860c3d5fca1fb157
Fauzan Salam Mutaqin <107753431+itsmejanz@users.noreply.github.com>
2026-02-14 18:14:14 +0700

    Rename function clicks to gatau

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-function clicks() {
+gatau clicks() {
     $('#div1').hide();
     $('#div2').show();
     $('#div2').addClass("fadeinv2");
         
     var typed = new Typed('.katakata', {
-        strings: ["Cuma mau bilang", "Selam malam", "ree gatau mau ngapain jujurr", "Happy Valentine's Dayy Weta Diyana Luna Liya"],
+        strings: ["Cuma mau bilang", "Selam malam", "gatau mau ngapain jujurr", "Happy Valentine's Dayy Weta Diyana Luna Liya"],
         typeSpeed: 70
     });
 
 }
+
 
